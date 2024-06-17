54d1f8f1be2cb6f7dca59944cb016c909b022cde
0xC00005 <0xC00005@gmail.com>
2024-06-17 17:05:40 -0400

    version 0.1.0
    change the google sheet backend to the MongoDB

---
+++ google_sheet/main.py
@@ -0,0 +1,189 @@
+# streamlit_app.py
+# database location: https://docs.google.com/spreadsheets/d/1NwOWe6aZkAjGFjkC39ibOXTseaoQvEY4aeNhGwpcdRQ/edit?usp=sharing\
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+from streamlit_extras.add_vertical_space import add_vertical_space
+import time
+import numpy as np
+
+st.set_page_config(
+    page_title="StreamPsy Human Experiments Demo",
+    page_icon="😎",
+    menu_items={
+        "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "https://www.extremelycoolapp.com/bug",
+        "About": "# This is a header. This is an *extremely* cool app!",
+    },
+)
+
+
+# return a connection object
+@st.cache_resource
+def init_connection():
+    # Create a connection object.
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    return conn
+
+
+conn = init_connection()
+
+# initial the session data `PageID`
+if "PageID" not in st.session_state:
+    st.session_state.page_id = 1
+
+
+def get_UserIDToPageID(conn: GSheetsConnection = conn):
+    return conn.read(
+        worksheet="UserIDToPageID",
+        ttl=0,
+        usecols=[0, 1, 2, 3],
+        nrows=3,
+    )
+
+
+def get_users_pageID(user_id: int, conn: GSheetsConnection = conn):
+    UserIDToPageID = get_UserIDToPageID(conn=conn)
+
+    # get the PageID of the UserID
+    page_id = UserIDToPageID.loc[0, "u" + str(user_id)]
+
+    return page_id
+
+
+def update_users_pageID(page_id: int, user_id: int, conn: GSheetsConnection = conn):
+    UserIDToPageID = get_UserIDToPageID(conn=conn)
+
+    # modify the first row of the column 'UserID' to the new PageID
+    UserIDToPageID.loc[0, "u" + str(user_id)] = page_id
+
+    # update the worksheet
+    conn.update(
+        worksheet="UserIDToPageID",
+        data=UserIDToPageID,
+    )
+
+    # set the session state to the new PageID
+    st.session_state.page_id = page_id
+
+
+if st.session_state.page_id == 1:
+    st.title("Welcome to the game")
+
+    # display the markdown instructions
+    st.markdown(
+        """
+        You can write your instructions in markdown. The app will render it for you.
+        
+        You can even put media here: 
+        """
+    )
+
+    # display a youtube video
+    st.video("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+    st.markdown(
+        """
+    ------
+    """
+    )
+
+    # choose the player number using a click-on then submit button
+    user_id = st.selectbox("What is your user ID?", ("1", "2", "3", "4"))
+
+    st.write("You selected:", user_id)
+
+    # move to the next PageID
+
+    if st.button("Submit & Next"):
+        st.session_state.UserID = user_id
+        previous_page_id = get_users_pageID(user_id=user_id, conn=conn)
+        if np.isnan(previous_page_id):
+            update_users_pageID(page_id=2, user_id=user_id, conn=conn)
+        else:
+            # turn PageID from np.float64 to int
+            update_users_pageID(
+                page_id=int(previous_page_id), user_id=user_id, conn=conn
+            )
+        st.rerun()
+
+
+if st.session_state.page_id == 2:
+    st.title("You are now at PageID 2")
+    st.write("You have selected player number: ", st.session_state.UserID)
+
+    if st.button("Go to PageID 3"):
+        update_users_pageID(page_id=3, user_id=st.session_state.UserID, conn=conn)
+        st.rerun()
+
+
+if st.session_state.page_id == 3:
+
+    st.title("You are now at PageID 3")
+    st.write("You have selected player number: ", st.session_state.UserID)
+    st.write("You have to wait at this page until all players are in page 3")
+
+    with st.spinner("Wait for it other users to reach page 3..."):
+        placeholder = st.empty()
+        while True:
+
+            num_user_reached = 0
+            UserIDToPageID = get_UserIDToPageID(conn=conn)
+            # check if all u1, u2, u3, u4 are in page 3
+            for i in range(1, 5):
+                if UserIDToPageID.loc[0, "u" + str(i)] >= 3:
+                    num_user_reached += 1
+            if num_user_reached >= 4:
+                break
+            else:
+                placeholder.empty()
+                placeholder.write(f"{num_user_reached}/4 users have reached page 3")
+
+            time.sleep(10)
+
+    update_users_pageID(page_id=4, user_id=st.session_state.UserID, conn=conn)
+    st.rerun()
+
+
+if st.session_state.page_id == 4:
+    st.title("You are now at PageID 4")
+    st.write("You have selected player number: ", st.session_state.UserID)
+
+    # post a example question
+
+    st.write("Question 1: Choose the following image that represent Will")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+        if st.button("Select", key="cat"):
+            # write a session state: selected
+            st.session_state.selected = "cat"
+
+
+    with col2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+        if st.button("Select", key="dog"):
+            # write a session state: selected
+            st.session_state.selected = "dog"
+
+
+    with col3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
+        st.button("Select", key="owl")
+
+        if st.button("Select", key="owl"):
+            # write a session state: selected
+            st.session_state.selected = "owl"
+
+
+    # display the selected image
+    if "selected" in st.session_state:
+        st.write("You selected the image of a", st.session_state.selected)
+    pass
+
+
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # streamlit_app.py
+from typing import Any, Mapping
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
-from streamlit_extras.add_vertical_space import add_vertical_space
 import time
-import numpy as np
+import pymongo
+from pymongo.database import Database
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
+    page_title="StreamPsy Human Experiments Demo",
+    page_icon="😎",
     menu_items={
         "Get Help": "https://www.extremelycoolapp.com/help",
         "Report a bug": "https://www.extremelycoolapp.com/bug",
@@ -18,57 +18,60 @@ st.set_page_config(
 
 # return a connection object
 @st.cache_resource
-def init_connection():
-    # Create a connection object.
-    conn = st.connection("gsheets", type=GSheetsConnection)
-    return conn
+def init_connection() -> Database[Mapping[str, Any] | Any]:
+    # connect to the MongoDB
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = client["SocialAI"]
+    return db
 
 
-conn = init_connection()
-
-# initial the session data `page_id`
-if "page_id" not in st.session_state:
-    st.session_state.page_id = 1
+CONN = init_connection()
 
 
-def get_UserIDToPageID(conn: GSheetsConnection = conn):
-    return conn.read(
-        worksheet="UserIDToPageID",
-        ttl=0,
-        usecols=[0, 1, 2, 3],
-        nrows=3,
-    )
+def get_UserIDToPageID(UserID: int, conn: Database[Mapping[str, Any] | Any] = CONN):
+
+    # read the UserIDtoPageID collection from the mongoDB
+    collection = conn["UserIDToPageID"]
+
+    # find the document with UserID, if the document does not exist, return None
+    document = collection.find_one({"UserID": UserID})
+
+    # return the PageID of the UserID
+    if document is not None:
+        return document["PageID"]
+    else:
+        return None
 
 
-def get_users_pageID(user_id: int, conn: GSheetsConnection = conn):
-    UserIDToPageID = get_UserIDToPageID(conn=conn)
+def update_users_pageID(
+    PageID: int, UserID: int, conn: Database[Mapping[str, Any] | Any] = CONN
+):
+    # update the PageID of the UserID
+    collection = conn["UserIDToPageID"]
 
-    # get the page_id of the user_id
-    page_id = UserIDToPageID.loc[0, "u" + str(user_id)]
+    # find the document with UserID
+    document = collection.find_one({"UserID": UserID})
 
-    return page_id
+    if document is None:
+        # if the document does not exist, insert a new document
+        collection.insert_one({"UserID": UserID, "PageID": PageID})
+    else:
+        # if the document exists, update the PageID
+        collection.update_one({"UserID": UserID}, {"$set": {"PageID": PageID}})
 
-
-def update_users_pageID(page_id: int, user_id: int, conn: GSheetsConnection = conn):
-    UserIDToPageID = get_UserIDToPageID(conn=conn)
-
-    # modify the first row of the column 'user_id' to the new page_id
-    UserIDToPageID.loc[0, "u" + str(user_id)] = page_id
-
-    # update the worksheet
-    conn.update(
-        worksheet="UserIDToPageID",
-        data=UserIDToPageID,
-    )
-
-    # set the session state to the new page_id
-    st.session_state.page_id = page_id
+    # set the session state to the new PageID
+    st.session_state.PageID = PageID
 
 
+# ============== Streamlit App ==============
 
 
-if st.session_state.page_id == 1:
-    st.title("Welcome to the game")
+# initial the session data `PageID`
+if "PageID" not in st.session_state:
+    st.session_state.PageID = 1
+
+if st.session_state.PageID == 1:
+    st.title("Welcome to the StreamPsy Human Experiments Demo")
 
     # display the markdown instructions
     st.markdown(
@@ -89,59 +92,94 @@ if st.session_state.page_id == 1:
     )
 
     # choose the player number using a click-on then submit button
-    user_id = st.selectbox("What is your user ID?", ("1", "2", "3", "4"))
+    UserID = st.selectbox("What is your UserID?", ("1", "2", "3", "4"))
+    UserID = int(UserID)
 
-    st.write("You selected:", user_id)
+    st.write("You selected:", UserID)
 
-    # move to the next page_id
+    # move to the next PageID
 
     if st.button("Submit & Next"):
-        st.session_state.user_id = user_id
-        previous_page_id = get_users_pageID(user_id=user_id, conn=conn)
-        if np.isnan(previous_page_id):
-            update_users_pageID(page_id=2, user_id=user_id, conn=conn)
+        st.session_state.UserID = UserID
+        PageID = get_UserIDToPageID(UserID=UserID, conn=CONN)
+        if PageID is None:
+            # the User hasn't logged in before, go to page 2
+            update_users_pageID(PageID=2, UserID=UserID, conn=CONN)
+            st.write(st.session_state.PageID)
         else:
-            # turn previous_page_id from np.float64 to int
-            update_users_pageID(
-                page_id=int(previous_page_id), user_id=user_id, conn=conn
-            )
+            # the User has logged in before, go to the page where the User left
+            st.session_state.PageID = PageID
+            st.write(st.session_state.PageID)
         st.rerun()
 
-if st.session_state.page_id == 2:
-    st.title("You are now at page_id 2")
-    st.write("You have selected player number: ", st.session_state.user_id)
+if st.session_state.PageID == 2:
+    st.title("You are now at PageID 2")
+    st.write("You have selected player number: ", st.session_state.UserID)
 
-    if st.button("Go to page_id 3"):
-        update_users_pageID(page_id=3, user_id=st.session_state.user_id, conn=conn)
+    if st.button("Go to PageID 3"):
+        update_users_pageID(PageID=3, UserID=st.session_state.UserID, conn=CONN)
         st.rerun()
 
-if st.session_state.page_id == 3:
+if st.session_state.PageID == 3:
 
-    st.title("You are now at page_id 3")
-    st.write("You have selected player number: ", st.session_state.user_id)
+    st.title("You are now at PageID 3")
+    st.write("You have selected player number: ", st.session_state.UserID)
     st.write("You have to wait at this page until all players are in page 3")
 
     with st.spinner("Wait for it other users to reach page 3..."):
         placeholder = st.empty()
         while True:
 
-            num_user_reached = 0
-            UserIDToPageID = get_UserIDToPageID(conn=conn)
-            # check if all u1, u2, u3, u4 are in page 3
+            num_of_user_have_reached_page_3 = 0
+            # check if userID 1, 2, 3, 4 are at least in page 3
             for i in range(1, 5):
-                if UserIDToPageID.loc[0, "u" + str(i)] == 3:
-                    num_user_reached += 1
-            if num_user_reached == 4:
+                if get_UserIDToPageID(i, conn=CONN) >= 3:
+                    num_of_user_have_reached_page_3 += 1
+            if num_of_user_have_reached_page_3 >= 4:
                 break
             else:
                 placeholder.empty()
-                placeholder.write(f"{num_user_reached}/4 users have reached page 3")
-            time.sleep(10)
+                placeholder.write(f"{num_of_user_have_reached_page_3}/4 users have reached page 3")
 
-    update_users_pageID(page_id=4, user_id=st.session_state.user_id, conn=conn)
+            time.sleep(3)
+
+    update_users_pageID(PageID=4, UserID=st.session_state.UserID, conn=CONN)
+
     st.rerun()
 
-if st.session_state.page_id == 4:
-    st.title("You are now at page_id 4")
-    st.write("You have selected player number: ", st.session_state.user_id)
+if st.session_state.PageID == 4:
+    st.title("You are now at PageID 4")
+    st.write("You have selected player number: ", st.session_state.UserID)
+
+    # post a example question
+
+    st.write("Question 1: Choose the following image that represent Will")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+        if st.button("Select", key="cat"):
+            # write a session state: selected
+            st.session_state.selected = "cat"
+
+    with col2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+        if st.button("Select", key="dog"):
+            # write a session state: selected
+            st.session_state.selected = "dog"
+
+    with col3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
+
+        if st.button("Select", key="owl"):
+            # write a session state: selected
+            st.session_state.selected = "owl"
+
+    # display the selected image
+    if "selected" in st.session_state:
+        st.write("You selected the image of a", st.session_state.selected)
     pass
